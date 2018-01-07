c05cb6d94504b927e9a09ddd378b09b89a9bb0f8
Connor Ford <cbfa2016@mymail.pomona.edu>
2018-01-07 09:10:11 -0800

    Cleaning

--- api_config.py
+++
@@ -1,7 +0,0 @@
-# API config
-# Connor Ford
-
-api_config = {'CONSUMER_KEY': 'GCMci1miMB1PcKsMjtxfAk2Cm',
-              'CONSUMER_SECRET': 'ZXmiuV3kPc1U47hr5y5WPN0PgLVP9frCIr653m4Gcxz997inat',
-              'ACCESS_TOKEN': '949394478745333760-azJLfuhYnv1LeFNkuDXb3SBvyRnC46w',
-              'ACCESS_TOKEN_SECRET' : 'UaUHarasgtWmfWN1IeqE4FtUQh8mDz0TTYFjvUjJSLw2r'}
